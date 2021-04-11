33f86ebf272f9746ef51f459cee91e84844f0247
Dwight Smith <dwightsmth@gmail.com>
2021-04-11 18:52:18 -0400

    reservation form and state refactors

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createReservation, updateReservation} from '../../actions/reservations';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { TextField, Button, Typography, Paper, Select, MenuItem /*InputLabel*/ } from '@material-ui/core';
 import useStyles from './styles';
+const availLocals  = ['rm01', 'rm02', 'rm03', 'rm04', 'rm05', 'rm06'];
+const availTimes  = ['8:30', '9:15', '10:00', '10:30', '11:45', '12:30'];
 
 const Form = ({currentId, setCurrentId}) => {
-    const [reservationData, setReservationData] = useState({student: '', itemName: '', cclNum: '', comments: ''});
+    const [reservationData, setReservationData] = useState({firstName: '', lastName: '', email: '', comments: '', reserveTime: '', reserveLocation: ''});
     const reservation = useSelector((state) => (currentId ? state.reservations.find((reservation) => reservation._id === currentId) : null));
     const dispatch = useDispatch();
     const classes = useStyles();
 
     useEffect(() => {
-        if (reservation) setReservationData(reservation);
-    }, [reservation]);
+        if (reservation) setReservationData(reservationData);
+    }, [reservation, reservationData]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -28,20 +30,40 @@ const Form = ({currentId, setCurrentId}) => {
 
     const clear = () => {
         setCurrentId(0);
-        setReservationData({student: '', itemName: '', cclNum: '', comments: ''});
+        setReservationData({firstName: '', lastName: '', email: '', comments: '', reserveTime: '', reserveLocation: ''});
     };
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant='h6'>{currentId ? "Editing A Reservation": 'Making A Reservation'}</Typography>
-                <TextField name='name' variant='outlined' label='Name' fullWidth value={reservationData.student} onChange={(e) => setReservationData({ ...reservationData, student: e.target.value })}/>
-                <Paper className={classes.reservedPaper}>
-                    <Typography>Reserved Items</Typography>
-                    <TextField name='itemName' variant='outlined' label='Item Name' fullWidth value={reservationData.itemName} onChange={(e) => setReservationData({ ...reservationData, itemName: e.target.value })}/>
-                    <TextField name='cclNum' variant='outlined' label='CCL #' fullWidth value={reservationData.cclNum} onChange={(e) => setReservationData({ ...reservationData, cclNum: e.target.value })}/>
+
+                {/* STUDENT INFO TEXT FIELDS */}
+                <TextField name='firstName' variant='outlined' label='First Name' fullWidth value={reservationData.firstName} 
+                onChange={(e) => setReservationData({ ...reservationData, firstName: e.target.value })}/>
+                <TextField name='lastName' variant='outlined' label='Last Name' fullWidth value={reservationData.lastName} 
+                onChange={(e) => setReservationData({ ...reservationData, lastName: e.target.value })}/>
+                <TextField name='email' variant='outlined' label='Email' fullWidth value={reservationData.email} 
+                onChange={(e) => setReservationData({ ...reservationData, email: e.target.value })}/>
+                <Paper>
+                    {/* <InputLabel id='reserveDateTime'>Select Time</InputLabel> */}
+                    <Select value={reservationData.reserveTime} id='reserveTime' onChange={(e) => setReservationData({...reservationData, reserveTime: e.target.value})}>
+                        { availTimes.map((l, i) => ( 
+                            <MenuItem key={i} id={`timeOpt-${i}`} value={l}>{l.toUpperCase()}</MenuItem> 
+                        ))}
+                    </Select>
+                    {/* <InputLabel id='reserveLocation'>Select Location</InputLabel> */}
+                    <Select value={reservationData.reserveLocation} id='reserveLocation' onChange={(e) => setReservationData({...reservationData, reserveLocation: e.target.value})}>
+                        { availLocals.map((l, i) => ( 
+                            <MenuItem key={i} id={`localeOpt-${i}`} value={l}>{l.toUpperCase()}</MenuItem> 
+                        ))}
+                    </Select>
                 </Paper>
-                <TextField name='comments' variant='outlined' label='Comments' fullWidth multiline rows={4} value={reservationData.comments} onChange={(e) => setReservationData({ ...reservationData, comments: e.target.value })}/>
+                {/* COMMENT SECTION  */}
+                <TextField name='comments' variant='outlined' label='Comments' fullWidth multiline rows={4} value={reservationData.comments} 
+                onChange={(e) => setReservationData({ ...reservationData, comments: e.target.value })}/>
+
+                {/* FORM SUBMIT AND CLEAR BUTTONS */}
                 <Button className={classes.buttonSubmit} variant='contained' color='primary' size="large" type='submit' fullWidth>Submit</Button>
                 <Button variant='contained' color='secondary' size='small' onClick={clear} fullWidth>Clear</Button>
             </form>
