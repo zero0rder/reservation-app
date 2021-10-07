7a814d33ae850414ded0584eb453e0cba58364be
Dwight Smith <dwightsmth@gmail.com>
2021-10-06 22:15:50 -0400

    update reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import reservations from './reservations';
+import studyrooms from './studyrooms';
 
-export const reducers = combineReducers({ reservations });
+export const reducers = combineReducers({ reservations, studyrooms });
