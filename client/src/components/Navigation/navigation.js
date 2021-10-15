70138baacbc55c49001e9c8c4a4dfbdbdc2efc35
Dwight Smith <dwightsmth@gmail.com>
2021-10-14 23:35:44 -0400

    refactor navigation header

--- client/src/components/Navigation/navigation.js
+++ client/src/components/Navigation/navigation.js
@@ -4,46 +4,44 @@ import { AppBar } from '@material-ui/core';
 import * as ROUTES from '../../constants/routes';
 import SignOutButton from './signout';
 import { AuthUserContext } from '../Session/index';
-// import useStyles from './styles';
+import useStyles from './styles';
 
-const Navigation = () => (
-    <AppBar position="static" color="inherit">
-        <AuthUserContext.Consumer>
-            { authUser => authUser ? <AuthenticatedUser/> : <NonAuthenticatedUser/> }
-        </AuthUserContext.Consumer>
-    </AppBar>
-    
-)
-
-const AuthenticatedUser = () => {
+const Navigation = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.ACCOUNT}>Account</Link>
-                </li>
-            </ul>
-            <SignOutButton />
-        </div>
-    );
+        <AppBar position="static" color="inherit" style={classes.header}>
+            <AuthUserContext.Consumer>
+                { authUser => authUser ? <AuthenticatedUser/> : <NonAuthenticatedUser/> }
+            </AuthUserContext.Consumer>
+        </AppBar>
+    )
 }
+
+const AuthenticatedUser = () => (
+    <>
+        <ul>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+        </ul>
+        <SignOutButton />
+    </>
+);
  
-const NonAuthenticatedUser = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to={ROUTES.HOME}>Home</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-                </li>
-            </ul>
-        </div>
-    );
-}
+const NonAuthenticatedUser = () => (
+    <>
+        <ul>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+        </ul>
+    </>
+);
 
 export default Navigation;
