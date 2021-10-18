0e3c97babee57ffdbb8dc8fa26da507977e9053c
Dwight Smith <dwightsmth@gmail.com>
2021-10-18 14:40:10 -0400

    disable main css import

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk';
 import { reducers } from './reducers';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import App from './App';
-import './index.css';
+//import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 ReactDOM.render(
