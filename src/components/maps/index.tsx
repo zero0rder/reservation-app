be37706955c34c1c01713144dbd0955f11b8ad7a
Dwight Smith <dwightsmth@gmail.com>
2022-11-18 19:20:06 -0500

    removal

--- src/components/maps/index.tsx
+++ src/components/maps/index.tsx
@@ -6,7 +6,7 @@ export const ReservationMap: React.FC<MapProps> = ({ geostate }) => {
     return (
         <Col span={24} style={{ textAlign: 'center', margin: '2rem' }}>
             { geostate !== null 
-            ? <img src={`https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=600x300&maptype=roadmap&markers=color:red%7C${geostate.lat},${geostate.lng}&key=AIzaSyCHjLmTXwcCSL-4E5P9qsBR9lpboJtA8ro`} /> 
+            ? <img src={`https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=600x300&maptype=roadmap&markers=color:red%7C${geostate.lat},${geostate.lng}&key={}`} /> 
             : <Spin/>}
         </Col>
     )
