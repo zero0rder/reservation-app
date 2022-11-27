d77bfa3f5ad583fd7fef18871bc0606cb878a8fb
Dwight Smith <dwightsmth@gmail.com>
2022-11-27 17:07:30 -0500

    config files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/common/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+    },
+  },
+  plugins: [],
+}
