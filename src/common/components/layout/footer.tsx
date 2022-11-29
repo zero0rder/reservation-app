2fa53266e88864834f44ab7078ea0246a10a2e40
Dwight Smith <dwightsmth@gmail.com>
2022-11-29 18:53:37 -0500

    layout comps

--- src/common/components/layout/footer.tsx
+++ src/common/components/layout/footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { MdCopyright } from 'react-icons/md'
 // import styles from './Layout.module.scss'
 interface FooterProps {}
 
 export const AppFooter: React.FC<FooterProps> = ({}) => {
     return (
-        <footer className='flex justify-center items-center w-full h-20 border-t'>
-             <p className='flex justify-center items-center'>DinDin App</p>
+        <footer className='flex justify-center items-center w-full h-15 md:h-20 border-t flex-col p-2 md:p-4 text-sm'>
+             <p className='flex justify-center items-center font-bold'>DinDin App</p>
+            <span className='italic'>All Rights Reserved <MdCopyright className='inline mb-[0.1rem]'/> {new Date().getFullYear()}</span>
         </footer>
     )
 }
--- src/common/components/layout/header.tsx
+++ src/common/components/layout/header.tsx
@@ -4,8 +4,8 @@ interface HeaderProps {}
 
 export const AppHeader: React.FC<HeaderProps> = ({}) => {
     return (
-        <header className='flex justify-center items-center w-full h-14 border-b'>
-             <span className='font-bold'>DinDin</span>
+        <header className='flex justify-center items-center w-full h-14 border-b p-4'>
+             <span className='font-bold text-2xl'>DinDin</span>
         </header>      
     )
 }
